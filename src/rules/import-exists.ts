4e6ea4b57b525f20f9f0993be8bc16ed67c8a79d
Erik Sundell <erik.sundell87@gmail.com>
2024-12-06 12:56:09 +0100

    copy over some levitate code

--- src/rules/import-exists.ts
+++ src/rules/import-exists.ts
@@ -2,26 +2,31 @@ import { RuleCreator } from '@typescript-eslint/utils/eslint-utils';
 import path from 'path';
 import fs from 'fs';
 import { Worker, MessageChannel, receiveMessageOnPort } from 'worker_threads';
-import { getExportInfo } from './utils';
+import semver from 'semver';
 export { ESLintUtils } from '@typescript-eslint/utils';
+import { getExportInfo } from './utils';
+
+const pluginJson = require(process.cwd() + '/src/plugin.json');
+const minVersion = semver.minVersion(pluginJson.dependencies.grafanaDependency);
 
 const { port1: localPort, port2: workerPort } = new MessageChannel();
 const shared = new SharedArrayBuffer(4);
 const workerData = { shared, port: workerPort };
-// the bundle does currently not include the worker code module, so this is a temporary workaround for that
-const workerModule = fs.existsSync(path.join(__dirname, '/rules/installPackages.js'))
-  ? path.join(__dirname, '/rules/installPackages.js')
-  : path.join(__dirname, '/installPackages.js');
-new Worker(workerModule, {
+// the bundle does currently not include the worker module, so this is a temporary workaround for that
+const workerFile = fs.existsSync(path.join(__dirname, '/rules/installPackages.js'))
+  ? '/rules/installPackages.js'
+  : '/installPackages.js';
+new Worker(path.join(__dirname, workerFile), {
   workerData,
   transferList: [workerPort],
 });
 const int32 = new Int32Array(shared);
-console.log('Installing lower bound of grafana packages in worker thread...');
+console.log(`Installing version ${minVersion} of grafana packages...`);
 Atomics.wait(int32, 0, 0);
-console.log('Done!');
 
-const packagePaths: Record<string, string> = receiveMessageOnPort(localPort)?.message;
+const { packagePaths, message }: { packagePaths: Record<string, string>; message: string } =
+  receiveMessageOnPort(localPort)?.message;
+console.log(message);
 
 const packageExports: Record<string, string[]> = {};
 Object.entries(packagePaths).forEach(([pkg, path]) => {
@@ -39,7 +44,8 @@ export const importExists = createRule<[], MessageIds>({
     },
     hasSuggestions: true,
     messages: {
-      'issue:import': 'The member "{{member}}" is not available in all runtime environments that this plugin supports.',
+      'issue:import':
+        'The member "{{member}}" is not available in all runtime environments that this plugin supports. Make sure to check if the member is undefined before accessing it, or it may cause runtime errors.',
     },
     schema: [
       {
@@ -57,7 +63,7 @@ export const importExists = createRule<[], MessageIds>({
         if (node?.imported?.name) {
           // @ts-ignore
           const from = node.parent.source.value as string;
-          if (from in packagePaths) {
+          if (from in packagePaths && packagePaths[from].length > 0) {
             const exportInfo = getExportInfo(packagePaths[from]);
             // const runtimeExports = getRuntimeExports(exportInfo.exports);
             const exports = Object.keys(exportInfo.exports).sort();
