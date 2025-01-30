f3e059db5516c8234daefe213351a4976ca5f51d
Jack Westbrook <jack.westbrook@gmail.com>
2025-01-30 17:52:20 +0100

    refactor(downloadpackages): rewrite js file to ts

--- src/rules/workers/downloadPackages.ts
+++ src/rules/workers/downloadPackages.ts
@@ -1,23 +1,23 @@
-const fs = require('fs');
-const path = require('path');
-const workerData = require('worker_threads').workerData;
-const fetch = require('node-fetch-commonjs');
-const os = require('os');
-const { spawnSync } = require('child_process');
-const { extract } = require('tar/x');
+import fs from "fs";
+import path from "path";
+import { workerData } from "worker_threads";
+import fetch from "node-fetch-commonjs";
+import os from "os";
+import { spawnSync } from "child_process";
+import { extract } from "tar/x";
 
-/* 
+/*
  This worker tread receives the package version from the main thread and returns the path to the packages on disk.
  If a package has been downloaded before, it will be cached and not downloaded again.
 
- Large chunks of this code are copied from the Levitate code base. Why not use the Levitate code directly? 
+ Large chunks of this code are copied from the Levitate code base. Why not use the Levitate code directly?
  Levitate doesn't support cjs modules which is required for the worker thread.
  */
 
 const { shared, port, packageVersion } = workerData;
 const shouldCacheExternal = true;
 
-function pathExists(path) {
+function pathExists(path: string) {
   try {
     fs.accessSync(path, fs.constants.R_OK);
     return true;
@@ -26,24 +26,26 @@ function pathExists(path) {
   }
 }
 
-function getTmpFolderName(packageName) {
-  return path.resolve(path.join(os.tmpdir(), packageName));
+function getTmpFolderName(packageName: string) {
+  return path.resolve(
+    path.join(os.tmpdir(), "grafana-eslint-compatible", packageName)
+  );
 }
 
-async function getPackageTarBallUrl(packageName) {
-  const { stdout } = spawnSync('npm', ['view', packageName, 'dist.tarball']);
+async function getPackageTarBallUrl(packageName: string) {
+  const { stdout } = spawnSync("npm", ["view", packageName, "dist.tarball"]);
 
   return stdout.toString().trim();
 }
 
-async function removeTmpFolder(packageName, ignoreCache = false) {
+async function removeTmpFolder(packageName: string, ignoreCache = false) {
   const tmpPackageFolder = getTmpFolderName(packageName);
   if (!shouldCacheExternal || ignoreCache) {
-    spawnSync('rm', ['-rf', tmpPackageFolder]);
+    spawnSync("rm", ["-rf", tmpPackageFolder]);
   }
 }
 
-async function createTmpPackageFolder(packageName) {
+async function createTmpPackageFolder(packageName: string) {
   const tmpPackageFolder = getTmpFolderName(packageName);
   await removeTmpFolder(packageName);
   fs.mkdirSync(tmpPackageFolder, { recursive: true });
@@ -51,49 +53,57 @@ async function createTmpPackageFolder(packageName) {
   return tmpPackageFolder;
 }
 
-async function downloadFile(url, path) {
+async function downloadFile(url: string, path: string) {
   const res = await fetch(url);
   const fileStream = fs.createWriteStream(path);
 
   await new Promise((resolve, reject) => {
-    res.body.pipe(fileStream);
-    res.body.on('error', reject);
-    fileStream.on('finish', resolve);
+    if (res && res.body) {
+      res.body.pipe(fileStream);
+      res.body.on("error", reject);
+      fileStream.on("finish", resolve);
+    } else {
+      reject(new Error("Failed to download file"));
+    }
   });
 }
 
-async function downloadNpmPackageAsTarball(packageName) {
+async function downloadNpmPackageAsTarball(packageName: string) {
   const tmpFolderName = await createTmpPackageFolder(packageName);
   const url = await getPackageTarBallUrl(packageName);
 
   if (!url) {
     await removeTmpFolder(packageName, true);
-    throw new Error(`Could not resolve package "${packageName}". Are you sure it exists?`);
+    throw new Error(
+      `Could not resolve package "${packageName}". Are you sure it exists?`
+    );
   }
 
   const tarballPath = path.join(tmpFolderName, path.basename(url));
-  const shouldDownload = !pathExists(tarballPath) || !(shouldCacheExternal && pathExists(tarballPath));
+  const shouldDownload =
+    !pathExists(tarballPath) ||
+    !(shouldCacheExternal && pathExists(tarballPath));
 
   if (shouldDownload) {
     await downloadFile(url, tarballPath);
     extract({ C: tmpFolderName, file: tarballPath, sync: true });
   } else {
-    console.log('\nUsing download cache. Flag passed: LEVITATE_CACHE=true');
+    console.log("\nUsing download cache. Flag passed: LEVITATE_CACHE=true");
   }
 
-  return path.join(tmpFolderName, 'package');
+  return path.join(tmpFolderName, "package");
 }
 
-function readJsonFile(path) {
+function readJsonFile(path: string) {
   const content = fs.readFileSync(path);
   return JSON.parse(content.toString());
 }
 
-function getPackageJsonPath(packagePath) {
-  return path.join(packagePath, 'package.json');
+function getPackageJsonPath(packagePath: string) {
+  return path.join(packagePath, "package.json");
 }
 
-function getPackageJson(packagePath) {
+function getPackageJson(packagePath: string) {
   if (!pathExists(getPackageJsonPath(packagePath))) {
     return null;
   }
@@ -101,13 +111,16 @@ function getPackageJson(packagePath) {
   return readJsonFile(getPackageJsonPath(packagePath));
 }
 
-const TYPE_DEFINITION_FILE_NAME = 'index.d.ts';
-function getTypeDefinitionFilePath(folder) {
+const TYPE_DEFINITION_FILE_NAME = "index.d.ts";
+
+function getTypeDefinitionFilePath(folder: string) {
   const packageJson = getPackageJson(folder);
 
   // if available use the package.json property that references a type definition file
   if (packageJson) {
-    const typeProp = ['types', 'typings'].find((prop) => packageJson[prop] !== undefined);
+    const typeProp = ["types", "typings"].find(
+      (prop) => packageJson[prop] !== undefined
+    );
     if (typeProp) {
       const typeDefinitionFilePath = packageJson[typeProp];
       return path.join(folder, typeDefinitionFilePath);
@@ -117,19 +130,22 @@ function getTypeDefinitionFilePath(folder) {
   return path.join(folder, TYPE_DEFINITION_FILE_NAME);
 }
 
-async function resolvePackage(packageName) {
+async function resolvePackage(packageName: string) {
   const tmpFolder = getTmpFolderName(packageName);
 
   if (pathExists(tmpFolder)) {
-    message = `Package version ${packageVersion} resolved from cache`;
-    return getTypeDefinitionFilePath(path.join(tmpFolder, 'package'));
+    message = `Package version ${packageVersion} resolved from cache at ${tmpFolder}`;
+    return getTypeDefinitionFilePath(path.join(tmpFolder, "package"));
   }
 
   const installedPackagePath = await downloadNpmPackageAsTarball(packageName);
-  const typeDefinitionFilePath = getTypeDefinitionFilePath(installedPackagePath);
+  const typeDefinitionFilePath =
+    getTypeDefinitionFilePath(installedPackagePath);
 
   if (!pathExists(typeDefinitionFilePath)) {
-    const errorMsg = `Could not find type definition file at "${getTypeDefinitionFilePath(localPath)}"`;
+    const errorMsg = `Could not find type definition file at "${getTypeDefinitionFilePath(
+      installedPackagePath
+    )}"`;
     console.error(errorMsg);
     throw new Error(errorMsg);
   }
@@ -137,10 +153,10 @@ async function resolvePackage(packageName) {
   return typeDefinitionFilePath;
 }
 
-const packagePaths = {
-  '@grafana/ui': '',
-  '@grafana/data': '',
-  '@grafana/runtime': '',
+const packagePaths: Record<string, string> = {
+  "@grafana/ui": "",
+  "@grafana/data": "",
+  "@grafana/runtime": "",
 };
 
 let message = `Successfully installed version ${packageVersion}`;
@@ -148,8 +164,8 @@ let message = `Successfully installed version ${packageVersion}`;
 const installPackages = async () => {
   return Promise.all(
     Object.keys(packagePaths).map((key) => {
-      return new Promise((resolve) => {
-        resolvePackage(key + '@' + packageVersion)
+      return new Promise<void>((resolve) => {
+        resolvePackage(key + "@" + packageVersion)
           .then((p) => {
             packagePaths[key] = p;
             resolve();
