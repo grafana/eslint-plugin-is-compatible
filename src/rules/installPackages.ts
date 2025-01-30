e646a8d6bcec82c4ba99219c0a328c1f657d811e
Jack Westbrook <jack.westbrook@gmail.com>
2025-01-30 17:59:50 +0100

    refactor(installpackages): remove file check when importing worker

--- src/rules/installPackages.ts
+++ src/rules/installPackages.ts
@@ -1,18 +1,13 @@
-import fs from 'fs';
-import path from 'path';
-import { Worker, MessageChannel, receiveMessageOnPort } from 'worker_threads';
+import path from "path";
+import { Worker, MessageChannel, receiveMessageOnPort } from "worker_threads";
 
 export function installPackages(packageVersion: string) {
   const { port1: localPort, port2: workerPort } = new MessageChannel();
   const shared = new SharedArrayBuffer(4);
   const workerData = { shared, port: workerPort, packageVersion };
-
-  // the bundle does currently not include the worker module, so this is a temporary workaround for that
-  // todo: handle this in a better way
-  const workerFile = fs.existsSync(path.join(__dirname, '/rules/workers/downloadPackages.js'))
-    ? '/rules/workers/downloadPackages.js'
-    : '/downloadPackages.js';
-  new Worker(path.join(__dirname, workerFile), {
+  // This path looks odd because it's relative to index.js file where
+  // this code ends up being bundled to.
+  new Worker(path.join(__dirname, "rules/workers/downloadPackages"), {
     workerData,
     transferList: [workerPort],
   });
